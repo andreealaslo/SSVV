5c187c706142a0b6b19bc57ac18e9719b2aa6eb3
914_Laslo_Andreea-Lavinia <deea.laslo@yahoo.com>
2024-03-23 18:05:08 +0200

    .

--- src/test/java/test/TestClass.java
+++ src/test/java/test/TestClass.java
@@ -17,28 +17,25 @@ import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertFalse;
 
-//@RunWith(JUnit4.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class TestClass {
-    public  Service service;
+@RunWith(JUnit4.class)
 
-    @BeforeAll
-    public void setup() {
+public class TestClass {
+    public Service service;
+    public String filenameStudent = "fisiere/Studenti.xml";
+    public String filenameTema = "fisiere/Teme.xml";
+    public String filenameNota = "fisiere/Note.xml";
+
+
+    @Test
+    public void addStudentSuccess() {
+
         StudentValidator studentValidator = new StudentValidator();
         TemaValidator temaValidator = new TemaValidator();
-        String filenameStudent = "fisiere/Studenti.xml";
-        String filenameTema = "fisiere/Teme.xml";
-        String filenameNota = "fisiere/Note.xml";
-
         StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
         TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
         NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
         NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
         service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
-    }
-
-    @Test
-    public void addStudentSuccess() {
 
         String id = "test";
         String nume = "ana";
